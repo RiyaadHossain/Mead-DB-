4b88383345e112a3d8d919a768f5de7c8d7b2984
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-02-28 18:11:35 +0600

    Full project done

--- JS/app.js
+++ JS/app.js
@@ -5,18 +5,25 @@ const mealContainer = document.getElementById("meal-container");
 
 const searchMeal = () => {
   const inputValue = inputMeal.value;
-  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => printFood(data.meals));
-  inputMeal.value = "";
+  if (inputValue.length == "") {
+    mealContainer.innerHTML = `
+    <h1 class="text-center my-4 text-info"> Please write something.</h1>
+    `;
+  } else {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => printFood(data.meals));
+    inputMeal.value = "";
+  }
 };
 
 const printFood = (data) => {
+  
   if (!data) {
-    mealContainer.innerHTML = `<h2 class="my-3 text-center">No result Found</h2>`;
+    mealContainer.innerHTML = `<h1 class="text-center my-4 text-info"> No, result found</h1>`;
   } else {
-    mealContainer.textContent = "";
+    mealContainer.innerHTML = "";
     data?.forEach((foodItem) => {
       const div = document.createElement("div");
       div.innerHTML = `
@@ -26,7 +33,7 @@ const printFood = (data) => {
                 <h3 class="card-title">${foodItem.strMeal}</h3>
                 <h5>Category: ${foodItem.strCategory}</h5>
                 <h5>Origin: ${foodItem.strArea}</h5>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <button  onclick="seeDetails(${foodItem.idMeal})" class="btn btn-primary">See Details</button>
             </div>
         </div>
         `;
@@ -34,3 +41,26 @@ const printFood = (data) => {
     });
   }
 };
+
+
+const seeDetails = foodID => {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${foodID}`)
+  .then(alAmin => alAmin.json())
+  .then(data => printDetails(data.meals[0]))
+}
+
+const printDetails = singleItem => {
+  console.log(singleItem)
+  mealContainer.innerHTML = `
+  <div class="card" style="width: 18rem;">
+  <img src="${singleItem.strMealThumb}" class="card-img-top" alt="...">
+  <div class="card-body">
+      <h3 class="card-title">${singleItem.strMeal}</h3>
+      <h5>Category: ${singleItem.strCategory}</h5>
+      <h5>Origin: ${singleItem.strArea}</h5>
+      <p>${singleItem.strInstructions.slice(0, 150)}</p>
+      <a href="${singleItem.strYoutube}"  class="btn btn-primary">Watch Tutorial</a>
+  </div>
+</div>
+  `
+}
