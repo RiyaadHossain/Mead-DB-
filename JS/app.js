5d31a59758b891e222ef3a830b2c344caae972b7
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-02-28 13:45:06 +0600

    Basic Functinalaties done

--- JS/app.js
+++ JS/app.js
@@ -1,31 +1,36 @@
 // Get All Variables
-const inputMeal = document.getElementById('input-meal')
-const searchBtn = document.getElementById('search-btn')
-const mealContainer = document.getElementById('meal-container')
+const inputMeal = document.getElementById("input-meal");
+const searchBtn = document.getElementById("search-btn");
+const mealContainer = document.getElementById("meal-container");
 
 const searchMeal = () => {
-    const url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=fish'
-    fetch(url)
-    .then(res => res.json())
-    .then(data => printFood(data.meals))
-}
+  const inputValue = inputMeal.value;
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => printFood(data.meals));
+  inputMeal.value = "";
+};
 
-const printFood = data => {
-
-    data.forEach(foodItem => {
-        console.log(foodItem) 
-        const div = document.createElement('div')
-        div.innerHTML = `
+const printFood = (data) => {
+  if (!data) {
+    mealContainer.innerHTML = `<h2 class="my-3 text-center">No result Found</h2>`;
+  } else {
+    mealContainer.textContent = "";
+    data?.forEach((foodItem) => {
+      const div = document.createElement("div");
+      div.innerHTML = `
         <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="${foodItem.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <h3 class="card-title">${foodItem.strMeal}</h3>
+                <h5>Category: ${foodItem.strCategory}</h5>
+                <h5>Origin: ${foodItem.strArea}</h5>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
         </div>
-        `
-        mealContainer.appendChild(div)
+        `;
+      mealContainer.appendChild(div);
     });
-
-}
+  }
+};
